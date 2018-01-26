f00e2ffe84b977dcc6151d101184f308fb10c6b0
zouyajing <zouyajing@crop-ci.com>
2018-01-26 15:31:17 +0800

    提交demo

---
+++ Hello/HelloWorld.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Hello;
+/**
+ * Created by PhpStorm.
+ * User: zouyj
+ * Date: 2018/1/26
+ * Time: 15:26
+ */
+class HelloWorld
+{
+    public function say_hello()
+    {
+        echo 'Hello World';
+    }
+}
